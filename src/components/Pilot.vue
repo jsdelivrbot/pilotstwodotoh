a2458b5591d9f8d91ed339110ed416c4e4b07b17
Patrick Beedgen <patrick.beedgen@sovanta.com>
2018-06-03 14:43:27 +0200

    update pilot editor

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -1,26 +1,53 @@
 <template>
     <div>
-        <div>
-            <input placeholder="Name your Pilot" type="text">
+        <div class="row">
+            <input placeholder="Name your Pilot" v-model="name" type="text">
         </div>
-        <div>
-            <input placeholder="Initiative" type="Number" min="1" max="6">
+        <div class="row">
+            <input placeholder="Initiative" v-model="initiative" type="Number" min="1" max="6">
         </div>
-        <div>
-            <textarea placeholder="Abililty text of your pilot" name="pilotability" id="" rows="10"></textarea>
+        <div class="row">
+            <textarea placeholder="Abililty text of your pilot" v-model="ability" name="pilotability"></textarea>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Pilot'
+export default {
+  name: "Pilot",
+  data() {
+    return {
+      initiative: 1,
+      name: "",
+      ability: ""
+    };
+  },
+  watch: {
+    initiative: function(oldVal, newVal) {
+      this.updatePilot();
+    },
+    name: function(oldVal, newVal) {
+      this.updatePilot();
+    },
+    ability: function(oldVal, newVal) {
+      this.updatePilot();
     }
+  },
+  methods: {
+    updatePilot() {
+      this.$store.dispatch("updatePilot", {
+        name: this.name,
+        initiative: this.initiative,
+        ability: this.ability
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-    textarea[name="pilotability"]{
-        resize: none;
-        width: 100%;
-    }
+textarea[name="pilotability"] {
+  resize: none;
+  width: 100%;
+}
 </style>
