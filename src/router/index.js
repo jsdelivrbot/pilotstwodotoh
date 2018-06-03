56f2809fb7d4795c174750b47320be0f24945c7e
Patrick Beedgen <patrick.beedgen@sovanta.com>
2018-06-03 20:29:04 +0200

    added layout-settings view

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Editor from '@/components/Editor'
 import Pilot from '@/components/Pilot'
 import Ship from '@/components/Ship'
+import Layout from '@/components/Layout'
 
 Vue.use(Router)
 
@@ -27,6 +28,11 @@ export default new Router({
           path: "ship",
           name: "Ship", 
           component: Ship
+        },
+        {
+          path: "layout",
+          name: 'Layout',
+          component: Layout
         }
       ]
     }
