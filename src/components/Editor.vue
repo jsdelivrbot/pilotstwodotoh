f60350110ef44cf9faccde2ad3a7bc46bf32924f
Patrick Beedgen <patrick.beedgen@sovanta.com>
2018-06-03 15:05:03 +0200

    better mobile device support

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
         <div class="row">
-            <Settings class="column column-50"/>
-            <Preview class="column column-50"/>
+            <Settings class="column"/>
+            <Preview class="column"/>
         </div>
     </div>
 </template>
